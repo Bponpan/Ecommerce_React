15750585ef14cdcb12bec19d8260fe4e5b301632
ponpanB <151675683+Bponpan@users.noreply.github.com>
2024-01-19 21:23:32 +0700

    19.01.2567

---
+++ ecommerce/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function navbar() {
+  return (
+    <div>
+      hello
+
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+haaaaaa
+    </div>
+  );
+}
+
+export default App;
+
+
