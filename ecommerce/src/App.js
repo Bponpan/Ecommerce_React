61109cbc94b691106e90616e4b00ccdeb25d3b24
ponpanB <151675683+Bponpan@users.noreply.github.com>
2024-01-19 23:13:29 +0700

    19.01.2567 23:13

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
 
-function navbar() {
+function Home() {
   return (
     <div>
-      hello
+      <div id="nav">
+        <a id="text-nav1">Comouter</a>
+        <a id="text-nav">Home</a>
+        <a id="text-nav">Products</a>
+        <a id="text-nav">Contacts</a>
+      </div>
+
+      <div id="body1">
+      <h1 id="tx-bd1">Buy your</h1>
+      <h1 id="tx-bd2">Computer</h1>
+      
+
+      </div>
 
     </div>
   );
 }
+export default Home;
 
-function App() {
-  return (
-    <div>
-haaaaaa
-    </div>
-  );
-}
-
-export default App;
 
 
